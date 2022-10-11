2ef44550e9135b46d379bc24596ea415dfcb2279
Eric <EricBandur@gmail.com>
2022-10-11 16:30:11 -0500

    Send data to database and image to bucket

--- functions/send-to-database.js
+++ functions/send-to-database.js
@@ -15,64 +15,79 @@ const dotenv = require("dotenv").config();
 // }
 const whatSite = 'https://multidollar.company/';
 
+
+
 exports.handler = async (event, context) => {
 
 
   const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
     executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
-    headless: chromium.headless,
+    headless: true,
     ignoreHTTPSErrors: true,
 });
 
   const page = await browser.newPage();
 
+  const elementFound = async (ele) => {
+    try {
+
+      console.log("Element", ele);
+      return await page.$eval(ele, (el) => el.innerText);
+      
+    } catch (error) {
+      console.log("No queue at this time");
+        // numberInQueue = 0;
+        // blizzETA = "None";
+        // asOf = "N/A"
+    }
+
+  }
 
   try {
     
     await page.goto(whatSite);
 
     const screenshot = await page.screenshot();
-    let numberInQueue = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(1) > span', (el) => el.innerText);
-    let blizzETA = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(2) > span', (el) => el.innerText);
 
+      let numberInQueue = await elementFound('body > section:nth-child(1) > div > h2 > div:nth-child(1) > span');
+
+      console.log(numberInQueue);
+  
+      let blizzETA = await elementFound('body > section:nth-child(1) > div > h2 > div:nth-child(2) > span')
+
+  
+      let asOf = await elementFound('body > section:nth-child(1) > div > p');
+  
+      // let test1 = await elementFound('body > section:nth-child(233)');
+    
 
     await browser.close();
 
-    if(numberInQueue === null || blizzETA === null) {
-      numberInQueue = 0;
-      blizzETA = "None";
-    }
-
-      //send text to supbase
-      // const { data, error } = await supabase
-      // .from('benediction-queue')
-      // .update({
-      //   number_in_queue: numberInQueue,
-      //   blizzard_eta: blizzETA,
-      //   updated_at: new Date().toISOString().toLocaleString('en-US'),
-      // }).match({
-      //   id: 1,
-      // })
-      // console.log(data);
       let queueUrl = "";
       if(screenshot) {
+
+        console.log("Screenshot if statement");
         const { data, error } = await supabase
         .storage
-        .from('images')
-        .upload('public/', screenshot);
+        .from('public')
+        .upload('current-bene-queue.png', screenshot, {
+          upsert: true,
+          contentType: 'image/png'
+        });
   
-        console.log(data);
+        console.log("Line 75", data);
 
         if(error) {
           console.log(error);
         }
 
         if(data) {
-          console.log(data);
+          queueUrl = data.Key
+          console.log("Line 82", data);
       }
     }
+
+    console.log("This code ran");
 
       const { data, error } = await supabase
       .from("benediction-queue")
@@ -80,9 +95,14 @@ exports.handler = async (event, context) => {
         updated_at: new Date().toISOString().toLocaleString('en-US'),
         number_in_queue : numberInQueue,
         blizzard_eta: blizzETA,
-        queue_url: screenshot
+        as_of: asOf,
+        queue_url: queueUrl
+      })
+      .match({
+        id: 1
       })
 
+      
 
 
     return {
@@ -94,18 +114,12 @@ exports.handler = async (event, context) => {
     }
 
 
-  } catch (error) {
-  await browser.close();
-  console.log(error);
-  return {
-      statusCode: 500,
-      body: JSON.stringify({error: 'Failed'}),
-  }
-}   
-
-
-
-
-
-
+    } catch (error) {
+    await browser.close();
+    console.log(error);
+    return {
+        statusCode: 500,
+        body: JSON.stringify({error: 'Failed'}),
+    }
+  }   
 }
